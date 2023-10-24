e44ffedaa798b55f4956353b99c27a0339df7057
Matias Bernardi <matias.bernardi.1@gmail.com>
2023-10-24 16:06:32 -0300

    arraglo de error middleware

--- src/middlewares/validateDuplicated.js
+++ src/middlewares/validateDuplicated.js
@@ -4,8 +4,9 @@ export const validateDucplicate = async (request, reply, done) => {
   const title = request.body.title
   const repeatedProduct = await Product.findOne({ title })
 
-  if (repeatedProduct !== undefined) {
+  if (repeatedProduct) {
     return reply.code(400).send({ message: 'Producto repetido' })
   }
+
   done()
 }
